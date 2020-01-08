279b01ec8f54175d5737f90ce0d2cfc0a5891f65
Matt Ackard <mattackard@gmail.com>
2020-01-07 21:13:02 -0600

    Added functionality to allow for more commands to be passed in as arguments

--- main.go
+++ main.go
@@ -2,17 +2,29 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
 func main() {
 	//Retrieves the file name passed in as the first argument
-	var fileName string = os.Args[1]
+	var cmd string = os.Args[1]
 
+	//check the value of 1st arg to determine function to run
+	switch cmd {
+	case "create":
+		createFile(os.Args[2], os.Args[3])
+	default:
+		fmt.Printf("%s is not recognized as a command \n", os.Args[1])
+	}
+
+}
+
+func createFile(fileName, text string) {
 	//creates a text file in the project directory
 	//the underscore is an unused error variable return from Create()
 	f, _ := os.Create(fileName + ".txt")
 
 	//writes a string to the file using the reference created with Create()
-	f.WriteString(os.Args[2])
+	f.WriteString(text)
 }
