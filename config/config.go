05d63a15013edcf9d079abe83537173edf6cd76d
Matt Ackard <mattackard@gmail.com>
2020-01-10 13:55:53 -0600

    default config file created if no config is found

--- config/config.go
+++ config/config.go
@@ -51,6 +51,20 @@ func LoadConfig() (cfg Config) {
 //CreateNewConfig generates a new config.json file at ./config.json
 func CreateNewConfig() *os.File {
 	os.Create("config.json")
+	newConfig := Config{
+		Paths: Path{
+			Notes: "./",
+		},
+		Options: Option{
+			DateStamp:     true,
+			FileExtension: ".txt",
+		},
+	}
+	file, err := json.MarshalIndent(newConfig, "", "    ")
+	err = ioutil.WriteFile("config.json", file, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
 	//load config once it as been created
 	config, err := os.Open("config.json")
 	if err != nil {
