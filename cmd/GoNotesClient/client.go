e08bc343ebba52092589af0c33b21b66fe485122
Matt Ackard <mattackard@gmail.com>
2020-01-20 11:24:21 -0600

    reverting back to locally hosted files

--- cmd/GoNotesClient/client.go
+++ cmd/GoNotesClient/client.go
@@ -5,5 +5,5 @@ import "github.com/zserge/webview"
 //open a webview connection using the client.html file
 //css and js is loaded in through the js
 func main() {
-	webview.Open("GoNotes", "https://raw.githack.com/mattackard/project-0/master/cmd/GoNotesClient/client.html", 600, 700, true)
+	webview.Open("GoNotes", "file:///home/ubuntu/go/src/github.com/mattackard/project-0/cmd/GoNotesClient/client.html", 600, 700, true)
 }
